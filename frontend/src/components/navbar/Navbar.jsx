88cdc291fa2d227598b3ec0a648f652d52dc8208
Nadya Chuchuganova <nadyav1807@gmail.com>
2022-12-13 18:35:12 +0200

    Работа със CSS

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,9 +2,10 @@ import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div id= "wrapper" className="navbar">
+       <a href="/"><img src= "https://media.discordapp.net/attachments/1034810869860806669/1052227508755497040/Blue_and_White_Minimalist_Travel_App_Logo.png"  alt="Wondrous Logo"/></a>
       <div className="navContainer">
-        <span className="logo">ProjectHotel</span>
+        <span className="logo">Wondrous Travel App</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
@@ -15,3 +16,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+           
