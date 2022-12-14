4029b020470e73320ed88d2446df6e56f2683a0f
BojoPopov <Bojo_popov@abv.bg>
2022-12-14 10:33:59 +0200

    Релации

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,4 +1,9 @@
 import express from "express";
+<<<<<<< Updated upstream
+=======
+import {createRoom,deleteRoom,getRoom,getRooms,updateRoom,} from "../controllers/room.js";
+import { verifyAdmin } from "../utilities/verifyToken.js";
+>>>>>>> Stashed changes
 
 const router = express.Router();
 
