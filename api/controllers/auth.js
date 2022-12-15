f5d0797261e796de1019464fc8663ca2642edb1b
BojoPopov <Bojo_popov@abv.bg>
2022-12-15 21:07:17 +0200

    Admin и проверка на грешки

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -8,7 +8,7 @@ export const register = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const newUser = new User({username: req.body.username, email: req.body.email, password:req.body,password: hash,});
+    const newUser = new User({...req.body,password: hash,});
 
     await newUser.save();
     res.status(200).send("User has been created.");
@@ -21,7 +21,7 @@ export const login = async (req, res, next) => {
     const user = await User.findOne({ username: req.body.username });
     if (!user) return next(createError(404, "User not found!"));
 
-    const isPasswordCorrect = await bcrypt.compare(req.body.password,user.password);
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
 
