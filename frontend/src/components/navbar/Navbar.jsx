6e97fc1846b682b3f0fd26986b7fa9f88502f5ec
Vilkata <106810456+Vilkata@users.noreply.github.com>
2022-12-16 09:23:31 +0200

    fixes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import "./navbar.css"
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
 
 const Navbar = () => {
 
   const {user} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSearch = () => {navigate("/login")}
   return (
-    <div className="navbar">
+    <div id= "wrapper" className="navbar">
+       <a href="/"><img src= "https://media.discordapp.net/attachments/1034810869860806669/1052227508755497040/Blue_and_White_Minimalist_Travel_App_Logo.png"  alt="Wondrous Logo"/></a>
       <div className="navContainer">
-        <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
-        <span className="logo">ProjectHotel</span>
-        </Link>
+        <span className="logo">Wondrous Travel App</span>
         {user ? user.username :
          (<div className="navItems">
           <button className="navButton">Регисрирай се</button>
-          <button className="navButton">Вход</button>
+          <button className="navButton" onClick={handleSearch}>Вход</button>
         </div>)}
       </div>
     </div>
