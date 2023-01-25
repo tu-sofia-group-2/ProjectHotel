34497f87798ee02e278b1937317d8b587e1ee77e
Katerina1203 <katie_g@abv.bg>
2023-01-26 01:21:37 +0200

    added forgotten password frontend and routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/List/List";
 import Login from "./pages/login/Login";
+import ForgottenPass from "./pages/login/ForgottenPass";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path = "/hotels" element = {<List/>}/>
         <Route path = "/hotels/:id" element = {<Hotel/>}/>
         <Route path = "/login" element = {<Login/>}/>
+        <Route path = "/forgotPassword" element = {<ForgottenPass/>}/>
       </Routes>
     </BrowserRouter>
   );
